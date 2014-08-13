5d3b20d7a5ba5201414ef48aa328d91a40f16126
mobeets <mobeets@gmail.com>
2014-08-13 15:11:57 -0500

    images

--- matlab/main.m
+++ matlab/main.m
@@ -27,8 +27,8 @@ Rh2 = rankreg(S, R, nLags, 1);
 Rh3 = rankreg(S, R, nLags, 2);
 
 %% fit (rank-2), manually minimizing hyperparameter for regularizer
-lambda = optimizeHyperparameter(S, R, nLags, 2, [1e-3 1e-3]);
-Rh3b = rankreg(S, R, nLags, 2, lambda);
+% lambda = optimizeHyperparameter(S, R, nLags, 2, [1e-3 1e-3]);
+% Rh3b = rankreg(S, R, nLags, 2, lambda);
 
 %% fit (full rank), using fixed-point ridge regression
 
@@ -40,8 +40,8 @@ Rh4b = rankreg(S, R, nLags, Inf, 'ARD');
 
 %% fit (full rank), manually minimizing hyperparameter for regularizer
 
-lambda = optimizeHyperparameter(S, R, nLags, Inf, 1e-3);
-Rh4c = rankreg(S, R, nLags, Inf, lambda);
+% lambda = optimizeHyperparameter(S, R, nLags, Inf, 1e-3);
+% Rh4c = rankreg(S, R, nLags, Inf, lambda);
 
 %% write results
 
@@ -52,14 +52,58 @@ disp(['rmse (rank-2, manual ridge) = ' num2str(rmse(R, Rh3b))]);
 disp(['rmse (full rank, ridge fixed point) = ' num2str(rmse(R, Rh4))]);
 disp(['rmse (full rank, ARD fixed point) = ' num2str(rmse(R, Rh4b))]);
 
+%% plot stimulus
+
+figure(8); clf; hold on;
+set(gca,'FontSize', 14);
+plot(S, 'k-', 'MarkerSize', 5);
+xlabel('time');
+ylabel('stimulus');
+
+%% plot response
+
+figure(9); clf; hold on;
+set(gca,'FontSize', 14);
+plot(R, 'k-', 'MarkerSize', 5, 'MarkerFaceColor', 'k', 'LineWidth', 1);
+% plot([1 numel(R)], [mean(R) mean(R)], 'r--', 'LineWidth', 1.2);
+xlabel('time');
+ylabel('response');
+
+%% plot stimulus vs. response
+
+figure(10); clf; hold on;
+set(gca,'FontSize', 14);
+scatter(S(nLags:end), R, 5, 'k', 'filled');
+xlabel('stimulus');
+ylabel('response');
+
 %% plot
 
-figure(6); clf; hold on;
-plot(R, 'k');
+figure(11); clf; hold on;
+
+sz = 30;
+% scatter(R, (R - Rh1), 4, 'yo');
+% scatter(R, (R - Rh2), sz, 'go', 'filled');
+scatter(R, (R - Rh3), sz, 'bo', 'filled');
+scatter(R, (R - Rh4), sz, 'ro', 'filled');
+
+% scatter(mean(R), mean(R-Rh1), 20, 'ko');
+% scatter(mean(R), mean(R-Rh2), 20, 'ko', 'filled');
+scatter(mean(R), mean(R-Rh3), 20, 'ko', 'filled');
+scatter(mean(R), mean(R-Rh4), 20, 'ko', 'filled');
+
+set(gca,'FontSize', 14);
+xlabel('response');
+ylabel('residual');
+% legend('linear', 'bilinear', 'rank-2', 'full rank', 'Location', 'SouthEast');
+legend('rank-2', 'full rank', 'Location', 'SouthEast');
+% figure(7); hist(R, 30);
+
+%% plot
 % plot(Rh1, 'c');
 % plot(Rh2, 'b');
 % plot(Rh3, 'r');
-plot(Rh4, 'b');
-plot(Rh4b, 'r');
+% plot(Rh4, 'b');
+% plot(Rh4b, 'r');
 title('response');
 
--- matlab/resp.m
+++ matlab/resp.m
@@ -9,7 +9,7 @@ function R = resp(S, nLags, nRank)
 
     % input nonlinearity, noise, constant
     f1 = @(x) 1e-2*x.^3;
-    f2 = @(x) 1e-1*x.^2;
+    f2 = @(x) 1*x.^2;
     noisesigma = 1;
     c = 250;
 
@@ -19,7 +19,8 @@ function R = resp(S, nLags, nRank)
     elseif nRank == 2
         R = w_true(5,1)'*f1(X) + w_true(2,1)'*f2(X) + c + randn(1, size(X, 2))*noisesigma;
         ploti(1, S, w_true(5,1), f1);
-        ploti(2, S, 10*w_true(2,1), f2);
+        ploti(2, S, w_true(2,1), f2);
+        set(gca,'FontSize', 14);
         figure(3); plot(R, 'k-', 'MarkerSize', 2); xlabel('time'); ylabel('spike rate');
     end
     
@@ -33,23 +34,26 @@ function ploti(i, S, w_true, f)
 %     plot(S, 'k')
 %     title('stimulus')
 
-    subplot(131)
+    subplot(131);
+    set(gca,'FontSize', 12);
     stim_bounds = linspace(min(S), max(S), 10);
     plot(stim_bounds, f(stim_bounds), 'ko', 'MarkerFaceColor', 'k');
     title('b, input nonlinearity');
     xlabel('stimulus value');
-    ylabel('response');
+    ylabel('weight');
 
     subplot(132)
+    set(gca,'FontSize', 12);
     plot(0:numel(w_true)-1, w_true, 'ko', 'MarkerFaceColor', 'k');
     xlim([0-0.5, numel(w_true)-0.5]);
     title('w, stimulus history weights');
-    xlabel('history');
-    ylabel('response');
+    xlabel('stimulus history');
+    ylabel('weight');
 
     subplot(133)
+    set(gca,'FontSize', 12);
     image(w_true*f(stim_bounds));
-    xlabel('nonlinearity response');
-    ylabel('history response');
+    xlabel('nonlinearity');
+    ylabel('history');
     title('C');
 end
